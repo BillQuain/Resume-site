25e9556dda46c1033a1701f3f31480ebbd15a26e
BillQuain <billyq151515@gmail.com>
2025-01-16 16:04:24 -0600

    added tactical chicken story

--- main.py
+++ main.py
@@ -60,6 +60,32 @@ st.write("""After college I decided to take a gap year to study other areas of c
             like C and C++, computer organization and architecture, Python development, and many other skills that I wanted to have.""")
 
 st.divider()
+
+st.header("Tactical Chicken development story")
+st.write("""
+        Tactical Chicken was a capstone project for my CS491 and CS492 software engineering course. My team consisted of me
+        and my good friend Tim Worthem. We were given total freedom by our professor to make any project we wanted as long as it
+        was in a language he knew and we followed software engineering practices.
+        \n I knew I wanted to make a game and I had heard of a simple game library called Pygame so I convinced Tim to work on a game with me for the project. 
+        Challenges immediately sprung up as Tim didn't know Python ,so I had to teach him that, and neither of us had developed any kind of video game before. 
+        However I was resourceful and found a heap of content online related to making all kinds of games with Python and Pygame.
+        \nInitially the idea for Tactical Chicken was for it to be a side scroller like Mario or the old Duke Nukem games. However neither of us had 
+        no idea how we would implement all of the things like projectiles, enemy AI, or map design. We eventually found tools so we could make our assets 
+        like characters and the map. I had the idea of making the enemies turkeys and that when the 
+        player shot the turkeys that their sprite would turn into a turkey dinner. I thought it was funny and it also made the game 
+        more light hearted. 
+        \nWe had some ambitious ideas for Tactical Chicken, but since I was also swamped with other classes and projects and 
+        Tim was double majoring in law enforcement time forced us to make the decision of what got cut from the final game. More challenges arose at the 
+        tail end of the project as I was sick and since Tim had to leave for Police academy we had to finish the project two weeks early! On the night we 
+        finished the project I had a migraine so bad I thought I was going to throw up. However I powered through it and we compiled the final version of Tactical 
+        Chicken and submitted it to our professor. When it came time to present I had to call in Tim over google meets so that he could also be part of the
+        presentation. Our professor was very impressed with the simple enemy AI and my resourcefulness in using google meets to have Tim present as well. 
+        I would argue we had the best project in the class in terms of technical achievements and challenges we faced 
+        \n Looking back Tactical Chicken may not be the best game made with Pygame, but I learned so much during this project. 
+        With having to finish the project early to having to learn entirely new technologies and 
+        presenting my work to others. The experience I gained from doing this project was an important step in my journey as a computer scientist.
+""")
+
 st.header("Screenshots of Tactical Chicken")
 st.image(os.path.join(os.getcwd(), "static", "menu.png"))
 st.image(os.path.join(os.getcwd(), "static", "gameplay.png"))
